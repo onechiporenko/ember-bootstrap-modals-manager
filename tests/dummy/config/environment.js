eaf41e8820d83011e3ac81aafc53677766c4194f
Oleg Nechiporenko <onechiporenko@apache.org>
2018-01-26 14:54:11 +0200

    Fixes for demo

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,6 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/ember-bootstrap-modals-manager/';
     // here you can enable a production-specific feature
   }
 
