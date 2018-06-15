d19efe2ea0ea36bc1da06cda2b64d150f75a05e8
Oleg Nechiporenko <onechiporenko@apache.org>
2018-06-15 18:53:29 +0300

    Fix demo-page

--- tests/helpers/last-log-message-assert.js
+++ tests/helpers/last-log-message-assert.js
@@ -1,6 +1,6 @@
 import { registerAsyncHelper } from '@ember/test';
 
 export default registerAsyncHelper('lastLogMessageAssert', function(app, assert, msg) {
-  const lastLogMsg = find('.logs').text().trim().split('\n').pop();
+  const lastLogMsg = find('.logs').html().trim().split('<br>').slice(0, -1).pop().trim();
   assert.equal(lastLogMsg, msg);
 });
