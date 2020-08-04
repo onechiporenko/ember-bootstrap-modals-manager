bd349cf2e9744b79ef30eef9e18f056c815e7800
onechiporenko <oo.nechiporenko@gmail.com>
2020-08-04 15:16:24 +0300

    Fix lint issue

--- tests/dummy/app/config/environment.d.ts
+++ tests/dummy/app/config/environment.d.ts
@@ -8,7 +8,7 @@ export default config;
  * since different ember addons can materialize new entries.
  */
 declare const config: {
-  environment: any;
+  environment: any;  // eslint-disable-line @typescript-eslint/no-explicit-any
   modulePrefix: string;
   podModulePrefix: string;
   locationType: string;
