00c40ac17a5dd36a3eaa214ec9d263cd36552cd3
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2022-06-23 18:18:20 +0300

    Try to fix embroider-safe

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,34 +1,24 @@
-import AddonDocsRouter, {
-  docsRoute,
-  apiRoute,
-} from 'ember-cli-addon-docs/router';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = AddonDocsRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL,
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function () {
-  docsRoute(this, function () {
-    this.route('install');
-
-    this.route('modals', function () {
-      this.route('alert');
-      this.route('confirm');
-      this.route('prompt');
-      this.route('check-confirm');
-      this.route('prompt-confirm');
-      this.route('process');
-      this.route('progress');
-      this.route('with-form');
-      this.route('passing-values');
-    });
-    this.route('sandbox', function () {
-      apiRoute(this);
-    });
+  this.route('install');
+  this.route('modals', function () {
+    this.route('alert');
+    this.route('confirm');
+    this.route('prompt');
+    this.route('check-confirm');
+    this.route('prompt-confirm');
+    this.route('process');
+    this.route('progress');
+    this.route('with-form');
+    this.route('passing-values');
   });
   this.route('demo');
+  this.route('docs');
 });
-
-export default Router;
