0244978718522833cb8f05b75ed3e80d67a7396d
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2023-07-14 17:53:50 +0300

    Move to ember-cli@5.0.0 (#75)

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -8,23 +8,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-            'ember-qunit': '~6.0.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
