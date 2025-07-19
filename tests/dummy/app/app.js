40bc70e6b60ad54856a78c0374430c2a944f0d28
Oleg Nechiporenko <root@TDK.localdomain>
2025-07-19 19:32:14 +0300

    Move to ember-cli@6.5.0

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,9 +1,13 @@
 import Application from '@ember/application';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import config from 'dummy/config/environment';
 import loadInitializers from 'ember-load-initializers';
 import { setup } from 'ember-prism';
 import Resolver from 'ember-resolver';
 
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
