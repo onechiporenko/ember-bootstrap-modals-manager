fb5738b954b7c49feb0bf3b6c3ce5c380f7fbf5d
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2025-10-05 14:35:43 +0300

    Move to ember-cli@6.7.0

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -5,7 +5,7 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
-    usePnpm: true,
+    packageManager: 'pnpm',
     scenarios: [
       {
         name: 'ember-lts-4.4',
@@ -54,6 +54,14 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~5.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-6.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.4.0',
           },
         },
       },
